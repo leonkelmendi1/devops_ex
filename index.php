02da2703f04ab3f6c5ae567b362af23b9b310496
Leon Kelmendi <leonkelmendi@Leons-Air.lan>
2021-10-06 16:46:04 +0200

    import du site des bonnes nouvelles

--- index.php
+++ index.php
@@ -1,3 +1,84 @@
 <?php
-echo "ok";
-?>
+	# Prise du temps actuel au début du script
+	$time_start = microtime(true);
+
+	# Variables globales du site
+	define('VIEW_PATH','views/');
+    define('EMAIL','jeanluc.collinet@ipl.be');
+	$date = date("j/m/Y");
+	
+	# Require des classes automatisé
+	function loadClass($class) {
+		require_once 'models/' . $class . '.class.php';
+	}
+	spl_autoload_register('loadClass');
+
+	# Ecrire ici le header de toutes pages HTML
+	require_once(VIEW_PATH . 'header.php');
+	
+	# Ecrire ici le menu du site de toutes pages HTML
+	require_once(VIEW_PATH . 'menu.php');
+
+	# Tester si une variable GET 'action' est précisée dans l'URL index.php?action=...
+	$action = (isset($_GET['action'])) ? htmlentities($_GET['action']) : 'default';
+	# Quelle action est demandée ?
+/**
+ * @return BooksController
+ */
+function extracted()
+{
+    require_once('controllers/BooksController.php');
+    $controller = new BooksController();
+    return $controller;
+}
+
+/**
+ * @return GenesisController
+ */
+function extracted1()
+{
+    require_once('controllers/GenesisController.php');
+    $controller = new GenesisController();
+    return $controller;
+}
+
+/**
+ * @return ContactController
+ */
+function extracted2()
+{
+    require_once('controllers/ContactController.php');
+    $controller = new ContactController();
+    return $controller;
+}
+
+/**
+ * @return HomeController
+ */
+function extracted3()
+{
+    require_once('controllers/HomeController.php');
+    $controller = new HomeController();
+    return $controller;
+}
+
+switch($action) {
+		case 'genesis':
+			$controller = extracted1();
+            break;
+		case 'books':
+			$controller = extracted();
+            break;
+		case 'contact':
+			$controller = extracted2();
+            break;
+		default: # Par défaut, le contrôleur de l'accueil est sélectionné
+			$controller = extracted3();
+            break;
+	}
+	# Exécution du contrôleur correspondant à l'action demandée
+	$controller->run();
+	
+	# Ecrire ici le footer du site de toutes pages HTML
+	require_once(VIEW_PATH . 'footer.php');
+	
